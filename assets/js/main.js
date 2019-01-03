3522b3467226b4b0642be1dbb6da91aa5c4e8833
matttali09 <matttaliancich09@gmail.com>
2019-01-03 00:11:24 -0500

    added contact page functionality

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// doc ready
+$(function () {
+    // submit button
+    $("#submit-btn").click(function (event) {
+        event.preventDefault();
+
+        let emailName = $("#name").val().trim();
+        let emailSub = $("#subject").val();
+        let emailBody = $("#message").val();
+
+        location.href = "mailto:matttaliancich09@gmail.com?subject=" + emailName + ": " + emailSub + "&body=" + emailBody;
+    })
+});
